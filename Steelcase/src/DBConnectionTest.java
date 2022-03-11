860d2dcd5b5af1bfab3ae9c930e536c69704a8b8
@EthanStyle <brownev19@gcc.edu>
2022-03-11 10:37:13 -0500

    Update DBConnectionTest.java
    Uses SA json key and environment variable to authorize connections with DB

--- Steelcase/src/DBConnectionTest.java
+++ Steelcase/src/DBConnectionTest.java
@@ -10,70 +10,48 @@
  *              run this class, contact me (Ethan) and we'll figure
  *              it out. 
  */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
-// import java.io.FileNotFoundException;
-// import java.io.FileInputStream;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.security.GeneralSecurityException;
 
-// import com.google.auth.oauth2.GoogleCredentials;
-// import com.google.common.collect.Lists;
-// import com.google.api.client.auth.oauth2.Credential;
-// import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-// import com.google.api.client.http.HttpTransport;
-// import com.google.api.client.json.Json;
-// import com.google.api.client.json.JsonFactory;
-// import com.google.api.services.sqladmin.SQLAdmin;
-// import com.google.cloud.sql.mysql.SocketFactory;
 
 public class DBConnectionTest {
   public static Connection conn;
 
-  // private final static String SA_KEY_PATH = "../ambient-scope-342219-3894e6e0fc09.json";
+  private final static String SA_ENV = "GOOGLE_APPLICATION_CREDENTIALS";
+  private final static String SA_KEY_ENV = System.getenv(SA_ENV);
 
   public static void main(String[] args) throws Exception {
+    if (SA_KEY_ENV == null) {
+      System.err.printf("The environment variable \"%s\" must be set to the " 
+        + "location of the service account json key.\n", SA_ENV);
+      System.exit(1);
+    }
     Properties info = new Properties();
 
     String user = "admin";
     String password = "admin";
     info.put("user", user);
     info.put("password", password);
-    String host = "34.122.211.163";
-    int port = 3306;
-    // String dbName = "steelcase";
-    // String cloudSqlInstance = "ambient-scope-342219:us-central1:steelcase-db";
-
-    // GoogleCredentials apiCredentials = GoogleCredentials.fromStream(new
-    // FileInputStream(SA_KEY_PATH))
-    // .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-
-    // SQLAdmin adminApiClient = createAdminApiClient();
-
+    String dbName = "steelcase";
+    String cloudSqlInstance = "ambient-scope-342219:us-central1:steelcase-db";
+    
     // jdbc:mysql:///<DATABASE_NAME>?cloudSqlInstance=<INSTANCE_CONNECTION_NAME>&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=<MYSQL_USER_NAME>&password=<MYSQL_USER_PASSWORD>
-    // String url = String.format(
-    //     "jdbc:mysql:///%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=%s&password=%s",
-    //     dbName, cloudSqlInstance, user, password);
-    String url = String.format("jdbc:mysql://%s:%d/",host,port);
-    System.out.println("This test whether or not this machine can connect "
-        + "to the Google Cloud SQL instance and database.");
+    String url = String.format("jdbc:mysql:///%s?cloudSqlInstance=%s"
+        + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=%s&password=%s",
+        dbName, cloudSqlInstance, user, password);
+    
+    System.out.println("This test whether or not this machine can connect " +
+        "to a mysql database hosted on the cloud.");
     try {
-      // conn = DriverManager.getConnection(url);
-      conn = DriverManager.getConnection(url, info);
-
+      conn = DriverManager.getConnection(url);
       System.out.println("Connection successful.");
     } catch (SQLException e) {
       System.out.println("Failed to connect.");
       e.printStackTrace();
-
-      System.out.println("This concludes the test.");
-      return;
     }
 
     try {
@@ -93,33 +71,10 @@ public class DBConnectionTest {
       rs1.close();
       showHostname.close();
     } catch (Exception e) {
-      System.err.println("Something exploded!!");
+      System.out.println("Something exploded");
       e.printStackTrace();
     }
-    System.out.println("This concludes the test.");
+    System.out.println("This concludes the test. Goodbye.");
     return;
   }
-
-  // private static SQLAdmin createAdminApiClient(Credential credential) {
-  // HttpTransport httpTransport;
-  // try {
-  // httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-  // } catch (GeneralSecurityException | IOException e) {
-  // throw new RuntimeException("Unable to initialize HTTP transport", e);
-  // }
-  // InputStream in;
-  // try {
-  // in = new FileInputStream(SA_KEY_PATH);
-  // } catch(FileNotFoundException e) {
-  // System.err.println("Couldn't find the Service Account Key file. "
-  // + "Make sure it's located in the project directory (eg.
-  // Steelcase/<key_file_here>.json)");
-  // e.printStackTrace();
-  // }
-  // JsonFactory jsf = GsonFactory.getDefaultInstance();
-  // return new SQLAdmin.Builder(httpTransport,
-  // JacksonFactory.getDefaultInstance(), credential)
-  // .setApplicationName("Cloud SQL Example")
-  // .build();
-  // }
 }
