81181b49a1da4cbaf84c3cb102352802b31e172f
CSColaneri <ColaneriCS19@gcc.edu>
2022-03-15 16:22:12 -0400

    basic driver setup

--- Steelcase/src/Driver.java
+++ Steelcase/src/Driver.java
@@ -1,37 +1,120 @@
 import java.util.*;
-import java.io.*;
-
 
 public class Driver {
-	public static void main(String argv[])
-	{
-		boolean running = true;
+	private boolean running = true;
+	private boolean loggedIn = false;
+
+	public static void main(String argv[]) {
+		Driver driver = new Driver();
+		driver.run();
+	}
+
+	public Driver(){
+
+	}
+
+	public void run() {
 		String in = "";
 		String help = "Commands:\n"
-				+ "help:  brings up help dialog\n"
-				+ "create: brings up the create schedule dialog\n"
-				+ ""
+				+ "help:  brings up help dialog.\n"
+				+ "create: brings up the create schedule dialog.\n"
+				+ "view: brings up dialog to view any schedules you have made.\n"
+				+ "login: brings up the login dialog"
+				+ "search: search for courses using the course search dialog"
+				+ "courses: a list of courses "
 				+ "\n";
-		String info = "";
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Welcome, User!");
 		System.out.println(help);
-		System.out.println(info);
-		while(running)
-		{
-			switch (in)
-			{
-				case "create":
-					break;
-				case "view":
-					break;
-				case "search":
-					break;
-				case "help":
+		if (loggedIn) {
+			while (running) {
+				in = input.next();
+				switch (in) {
+					case "create":
+						createSchedulePage();
+						break;
+					case "view":
+						viewSchedulePage();
+						break;
+					case "search":
+						searchCoursesPage();
+						break;
+					case "help":
+						System.out.println(help);
+						break;
+					case "logout":
+						logoutPage();
+						break;
+					case "exit":
+						running = false;
+						break;
+					default:
+						System.out.println("No command found: " + in);
+						break;
+				}
+			}
+		} else {
+			while (running) {
+				in = input.next();
+				switch (in) {
+					case "create":
+						createSchedulePage();
+						break;
+					case "view":
+						viewSchedulePage();
+						break;
+					case "search":
+						searchCoursesPage();
+						break;
+					case "help":
+						System.out.println(help);
+						break;
+					case "logout":
+						loginPage();
+						break;
+					case "exit":
+						running = false;
+						break;
+					default:
+						System.out.println("No command found: " + in);
+						break;
+				}
 			}
 		}
-		
+		input.close();
+	}
+
+	public void createSchedulePage() {
+
+	}
+
+	public void viewSchedulePage() {
+
+	}
+
+	public void searchCoursesPage() {
+
+	}
+
+	public void courseCatalogPage() {
+
+	}
+
+	public void loginPage() {
+
+	}
+
+	public void logoutPage() {
+
+	}
+
+	public void signupPage() {
+
+	}
+
+	public void accoutnDetailsPage(){
+
 	}
 
 }
