fa730535c37b223338ae29c20f764a769e5b9a6b
@EthanStyle <brownev19@gcc.edu>
2022-03-18 22:01:42 -0400

    Tentative Course Object Fields

--- Steelcase/src/Course.java
+++ Steelcase/src/Course.java
@@ -1,4 +1,41 @@
 
 public class Course {
+  // TODO: This can't be right...
+  private int id;
+  private int code;
+  private String department;
+  private char section;
+  private String building;
+  private String long_title;
+  private String short_title;
+  private String description;
+  private String professor;
+  private String day;
+  private String begin_time;
+  private String end_time;
+  private int capacity;
+  private int enrollment;
+  private String room;
+
+  public Course(int id, int code, String department, char section, String building, String long_title,
+      String short_title, String description, String professor, String day, String begin_time, String end_time,
+      int capacity, int enrollment, String room) {
+    this.id = id;
+    this.code = code;
+    this.department = department;
+    this.section = section;
+    this.building = building;
+    this.long_title = long_title;
+    this.short_title = short_title;
+    this.description = description;
+    this.professor = professor;
+    this.day = day;
+    this.begin_time = begin_time;
+    this.end_time = end_time;
+    this.capacity = capacity;
+    this.enrollment = enrollment;
+    this.room = room;
+  }
+  
 
 }
